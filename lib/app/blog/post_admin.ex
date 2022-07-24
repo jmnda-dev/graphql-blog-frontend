db75614bf0a8a34f8e4502c6d3a6eff5bb178608
Jachin Manda <jachin@outlook.com>
2022-07-24 09:27:48 +0200

    customize form fields

---
+++ lib/app/blog/post_admin.ex
@@ -0,0 +1,19 @@
+defmodule App.Blog.PostAdmin do
+  import Ecto.Query, warn: false
+
+  def before_insert(conn, changeset) do
+    {:ok,
+     changeset
+     |> Ecto.Changeset.put_assoc(:user, conn.assigns.current_user)}
+  end
+
+  def form_fields(_) do
+    [
+      title: %{type: nil},
+      excerpt: nil,
+      content: %{type: :richtext},
+      featured_image: nil,
+      published: %{type: :boolean}
+    ]
+  end
+end
