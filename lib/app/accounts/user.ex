fd2a7d1d2b102f336474cbdcadbae7e8ed908a85
Jachin <jachin.dev@outlook.com>
2022-08-20 13:15:25 +0200

    remove roles from required fields

--- lib/app/accounts/user.ex
+++ lib/app/accounts/user.ex
@@ -37,7 +37,7 @@ defmodule App.Accounts.User do
   def registration_changeset(user, attrs, opts \\ []) do
     user
     |> cast(attrs, [:first_name, :last_name, :username, :email, :password, :roles])
-    |> validate_required([:email, :password, :roles])
+    |> validate_required([:email, :password])
     |> unique_constraint(:email)
     |> validate_email()
     |> validate_password(opts)
