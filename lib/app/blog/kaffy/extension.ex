384ee73a6efc25cf3832baef2d1c502dd649c871
Jachin <jachin.dev@outlook.com>
2022-08-21 13:17:03 +0200

    disable markdown field

--- lib/app/blog/kaffy/extension.ex
+++ lib/app/blog/kaffy/extension.ex
@@ -1,16 +1,16 @@
 defmodule App.Blog.Kaffy.Extension do
   def stylesheets(_conn) do
-    [
-      {:safe,
-       ~s("<link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css">"})}
-    ]
+    # [
+    #   {:safe,
+    #    ~s("<link rel="stylesheet" href="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.css">"})}
+    # ]
   end
 
   def javascripts(_conn) do
-    [
-      {:safe,
-       ~s(<script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script>)},
-      {:safe, "<script>new SimpleMDE();</script>"}
-    ]
+    # [
+    #   {:safe,
+    #    ~s(<script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script>)},
+    #   {:safe, "<script>new SimpleMDE();</script>"}
+    # ]
   end
 end
