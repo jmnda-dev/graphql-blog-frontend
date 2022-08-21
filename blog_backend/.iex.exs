dd8f3ae81edb7980c6db9b7dbed66703a1d090d5
Jachin <jachin.dev@outlook.com>
2022-08-21 19:32:53 +0200

    remove backend folder

--- blog_backend/.iex.exs
+++
@@ -1,5 +0,0 @@
-alias App.Repo
-alias App.Accounts
-alias App.Accounts.{User, Profile}
-alias App.Blog
-alias App.Blog.{Post, Comment}
--- blog_backend/lib/app_web/resolvers/accounts.ex
+++
@@ -1,7 +0,0 @@
-defmodule AppWeb.Resolvers.Accounts do
-  alias App.Accounts
-
-  def get_author_profile(_parent, _, _resolution) do
-    {:ok, Accounts.author_profile!()}
-  end
-end
